f75146ef6e5242084e5261ef7d9e8c273e3cb747
ar-eeb1 <areebamir.pk@gmail.com>
2025-09-09 02:32:45 +0500

    Updated GitHub profile card with email toggle feature

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script src="https://cdn.lordicon.com/lordicon.js"></Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- app/page.js
+++ app/page.js
@@ -1,103 +1,115 @@
+"use client"
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { QRCodeCanvas } from "qrcode.react";
+import { toPng } from "html-to-image";
+import { useRef } from "react";
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [username, setUsername] = useState("")
+  const [name, setName] = useState()
+  const [followers, setFollowers] = useState(null)
+  const [bio, setBio] = useState("")
+  const [avatar, setAvatar] = useState()
+  const [email, setEmail] = useState("")
+  const [qr, setQr] = useState("")
+  const [iconSize, setIconSize] = useState(250)
+  const cardRef = useRef(false)
+  const emailRef = useRef()
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  async function downloadCard() {
+    const dataUrl = await toPng(cardRef.current)
+    const link = document.createElement("a")
+    link.href = dataUrl
+    link.download = 'card.png'
+    link.click()
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    console.log(username);
+    let res = await fetch(`https://api.github.com/users/${username}`)
+    let a = await res.json()
+
+    setName(a.name)
+    setFollowers(a.followers)
+    setBio(a.bio)
+    setAvatar(a.avatar_url)
+    setQr(`https://github.com/${username}`)
+    setEmail(a.email)
+    setIconSize(50)
+  }
+
+  return (
+    <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-gray-500 p-6">
+      
+    <div><h1 className="text-center text-7xl text-yellow-800 font-bold m-5">Create Your Git visiting Card</h1></div>
+
+      <form onSubmit={handleSubmit} className="flex gap-3 mb-8">
+        <input
+          type="text"
+          value={username}
+          placeholder="Enter GitHub ID"
+          onChange={(e) => setUsername(e.target.value)}
+          className="px-4 py-2 rounded-md border border-gray-400 focus:outline-none focus:ring-1 focus:ring-black"
+        />
+        <button
+          type="submit"
+          className="px-6 py-2 bg-black text-white rounded-md hover:bg-gray-800"
         >
+          Search
+        </button>
+      </form>
+      <div className="flex items-center justify-center text-center w-[300px] flex-col rounded-lg bg-black  shadow-md  overflow-hidden " ref={cardRef}>
+        <div className="w-full bg-gradient-to-r text-yellow-600 from-yellow-500 via-yellow-600 to-yellow-400 mb-2">.</div>
+        {avatar && (
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={avatar}
+            alt="GitProfile"
+            width={120}
+            height={120}
+            className="rounded-full border border-white p-2 mb-4"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        )}
+        <h1 className="text-xl font-bold bg-gradient-to-r from-yellow-500 via-yellow-600 to-yellow-600 bg-clip-text text-transparent">{name}</h1>
+        <h3 className="text-gray-100">Followers: {followers}</h3>
+        <p className=" text-gray-100 m-2">{bio}</p>
+        {
+          emailRef.current?.checked && (
+            <p className="text-gray-100 m-2">{email}</p>
+          )
+        }
+
+        {qr && (
+          <QRCodeCanvas
+            value={qr}
+            size={120}
+            bgColor="#ffffff"
+            fgColor="#000000"
+            level="H"
+            includeMargin={true}
+            className="rounded-xl m-3"
+          />
+        )}
+        <lord-icon
+          src="https://cdn.lordicon.com/acgiczyg.json"
+          trigger="loop"
+          delay="1500"
+          state="in-reveal"
+          colors="primary:#e8b730,secondary:#e88c30"
+          style={{ width: `${iconSize}px`, height: `${iconSize}px`, transition: "all 0.5s ease-in-out" }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </lord-icon>
+      </div>
+      <button onClick={downloadCard} className="bg-black text-sm text-white m-2 rounded-2xl pl-4 pr-4 cursor-pointer">Download PNG</button>
+      <div className="flex justify-center items-center gap-2">
+
+      <input type="checkbox" ref={emailRef} />
+        <label>Include E-mail</label>
+      </div>
     </div>
-  );
+
+
+  )
 }
